3707633c05543a44dbacc1efdc04b00c6af9e9f5
abhishek <ap7785980@gmail.com>
2026-07-16 22:59:23 +0530

    feat: implement user registration

--- Backend/controllers/auth.controller.js
+++ Backend/controllers/auth.controller.js
@@ -1,39 +1,58 @@
+const { validationResult } = require("express-validator");
+const bcrypt = require("bcrypt");
 const userModel = require("../models/user.model");
-const UserModel= require("../models/user.model");
-const {validationResult}= require("../validators/auth.validator");
-const bcrypt= require("bcrypt");
+const generateOtp = require("../utils/generateOtp");
+const sendMail = require("../utils/sendMail");
 const register= async (req,res,next) => {
      try {
-        const errors= validationResult(req);
-        if(!errors.isEmpty()){
+         const error= validationResult(req);
+         if(!error.isEmpty()){
             return res.status(400).json({
-                errors:errors.array()
-            });
-        }
-        
-        const{fullname,email ,password,profileImage}= req.body;
-        const existingUser= await userModel.findOne({
-            email
-        })
-       if(existingUser){
-          return res.status(400).json({
-            message:"already email exists"
-          })
-       }
-       const hashedPassword= await bcrypt.hash(password,10);
-       const otp= generateOtp();
-       const otpExpiry= new Date(Date.now()+10*60*1000);
-       const user= await userModel.create({
-           fullname,
-           email,
-           password,
-           profileImage
-       })
-       
+                error:error.array()
+            })
+         }
+        const{fullname,email,password}= req.body
+         const existinguser= await  userModel.findOne({
+             email
+         })
+         if(existinguser){
+             if(existinguser.isVerified==true){
+                return res.status(409).json({
+                    message:"already register"
+                })
+             }
+             if(existinguser.isVerified==false){
+                const otp= generateOtp();
+                const otpExpiry=  new Date(Date.now()+5*60*1000);
+                 existinguser.otp= otp;
+                 existinguser.otpExpiry=otpExpiry;
+                 await existinguser.save()
+                 await sendMail(email,"Email verification",`your otp is ${otp} this is valid for 5 minutes`);
+                 return res.status(200).json({
+                    message:"a new otp has been sent to  your email"
+                 })
+                }
+         }
+         const hashpassword= await bcrypt.hash(password,10);
+         const otp= generateOtp();
+         const otpExpiry= new Date(Date.now()+5*60*1000);
+         const user=  await userModel.create({
+            fullname,
+             email,
+             password:hashpassword,
+             otp,
+             otpExpiry
+         })
+         await sendMail(email,"Email verification",`your otp is ${otp} this is valid for 5 minutes`)
+         res.status(201).json({
+            message:"Registration successful. Please verify your email, using the OTP sent to your email."
+
+         })
      } catch (error) {
         next(error)
      }
-}
+} 
+    
 
 const verifyOtp= async (req,res,next) => {
      try {
