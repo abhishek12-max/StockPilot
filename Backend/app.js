46a65493627d4bb40e00ecb6bbf5c40901f8cc82
abhishek <ap7785980@gmail.com>
2026-07-16 17:21:06 +0530

    feat : mongdb connection

---
+++ Backend/app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(
+    cors({
+        origin: "http://localhost:5173",
+        credentials: true,
+    })
+);
+
+module.exports = app;
+
+
