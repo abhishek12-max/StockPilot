333dd314f45c5453ecc78f57801bb1dee9e69125
abhishek <ap7785980@gmail.com>
2026-07-21 11:40:29 +0530

    feat: update holdings after buy order

--- Backend/controllers/order.controller.js
+++ Backend/controllers/order.controller.js
@@ -1,6 +1,6 @@
 const Stock= require("../models/stock.model");
 const Order= require("../models/order.model");
-
+const  Holding= require("../models/holding.model");
 const placeOrder= async (req,res,next) => {
     try {
         const userId= req.user._id;
@@ -20,12 +20,37 @@ const placeOrder= async (req,res,next) => {
                side,
                orderType
          });
-         return res.status(201).json({
+         const existingHolding = await Holding.findOne({
+             user:userId,
+             stock:stockId
+         });
+         if(!existingHolding){
+            const holding= await Holding.create({
+                 user:userId,
+                 stock:stockId,
+                 quantity,
+                 averagePrice:price
+            })
+            return res.status(201).json({
+                 "success":true,
+                 message:"holding create",
+                 order,
+                 holding
+            });
+         }
+         const oldInvestment= existingHolding.quantity*existingHolding.averagePrice;
+         const newInvestment= quantity*price
+         const totalnvestment= oldinvestment+newinvestment;
+         const totalQuantity= existingHolding.quantity+quantity;
+         const newAverageprice=totalinvestment/totalquantity
+         existingHolding.quantity= totalquantity;
+         existingHolding.averagePrice= newaverageprice;
+         await existingHolding.save();
+         return res.status(200).json({
             "success":true,
-            message:"order placed successfully",
-            data:{
-                "order":order
-            }
+            message:"order placed  successfully",
+             order,
+            existingHolding
          })
     } catch (error) {
         next(error)
