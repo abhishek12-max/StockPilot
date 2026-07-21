006300c1b90f66222236e9ec4d20fd5a6fb48e8d
abhishek <ap7785980@gmail.com>
2026-07-21 13:00:30 +0530

    feat: implement buy and sell order workflow

--- Backend/controllers/order.controller.js
+++ Backend/controllers/order.controller.js
@@ -12,46 +12,72 @@ const placeOrder= async (req,res,next) => {
                 message:"stock not found"
             })
          }
-         const order= await Order.create({
-              user:userId,
-              stock: stockId,
+         if(side==="BUY"){
+              const existingHolding= await Holding.findOne({
+                user:userId,
+                stock:stockId
+              });
+              if(!existingHolding){
+                   const holding= await Holding.create({
+                       user:userId,
+                       stock:stockId,
+                       quantity,
+                       averagePrice:price
+                   });
+              }else{
+                   const oldInvestment= existingHolding.quantity*existingHolding.averagePrice;
+                   const newInvestment=  quantity*price
+                 const  totalquantity= existingHolding.quantity+quantity;
+                  const totalInvestment= oldInvestment+newInvestment;
+                const newAveragePrice= totalInvestment/totalquantity;
+                 existingHolding.quantity=  totalquantity;
+                existingHolding.averagePrice= newAveragePrice
+               await existingHolding.save();
+              }
+         }
+          else if(side==="SELL"){
+              const existingHolding= await Holding.findOne({
+                user:userId,
+                stock:stockId
+              });
+
+              if(!existingHolding){
+                  return res.status(400).json({
+                    "success":false,
+                    message:"you don't own this stock"
+                  })
+              }
+
+
+              if(existingHolding.quantity<quantity){
+                   return res.status(400).json({
+                    "success":false,
+                    message:"unsuffient shares"
+                  });
+              }
+
+              const newQuantity= existingHolding.quantity-quantity;
+              if(newQuantity===0){
+                  await existingHolding.deleteOne();
+              }else{
+                   existingHolding.quantity= newQuantity;
+                 await existingHolding.save();
+              }
+         }
+           
+         const Order= await Order.create({
+               user:userId,
+               stock:stockId,
                quantity,
                price,
                side,
                orderType
-         });
-         const existingHolding = await Holding.findOne({
-             user:userId,
-             stock:stockId
-         });
-         if(!existingHolding){
-            const holding= await Holding.create({
-                 user:userId,
-                 stock:stockId,
-                 quantity,
-                 averagePrice:price
-            })
-            return res.status(201).json({
-                 "success":true,
-                 message:"holding create",
-                 order,
-                 holding
-            });
-         }
-         const oldInvestment= existingHolding.quantity*existingHolding.averagePrice;
-         const newInvestment= quantity*price
-         const totalnvestment= oldinvestment+newinvestment;
-         const totalQuantity= existingHolding.quantity+quantity;
-         const newAverageprice=totalinvestment/totalquantity
-         existingHolding.quantity= totalquantity;
-         existingHolding.averagePrice= newaverageprice;
-         await existingHolding.save();
-         return res.status(200).json({
+         })   
+         return res.status(201).json({
             "success":true,
-            message:"order placed  successfully",
-             order,
-            existingHolding
-         })
+            message:"order placed successfully",
+            Order
+         });
     } catch (error) {
         next(error)
     }
