3d6b26b7d4b2d1138263ce01c3fc886a65f7e085
abhishek <ap7785980@gmail.com>
2026-08-06 15:41:49 +0530

    Fix Gmail SMTP configuration

--- Backend/config/mail.config.js
+++ Backend/config/mail.config.js
@@ -2,15 +2,15 @@ const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: 465,
-  secure: true,
+  port: 587,
+  secure: false, // 465 nahi
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
-  connectionTimeout: 30000,
-  greetingTimeout: 30000,
-  socketTimeout: 30000,
+  tls: {
+    rejectUnauthorized: false,
+  },
 });
 
 module.exports = transporter;
