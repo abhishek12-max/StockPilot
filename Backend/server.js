04820eb4ddb3cbc3fd23c7783c8ba49066e57551
abhishek <ap7785980@gmail.com>
2026-08-06 15:08:44 +0530

    Add error logging

--- Backend/server.js
+++ Backend/server.js
@@ -1,18 +1,29 @@
 require("dotenv").config();
 const app = require("./app");
 const connectDB = require("./config/db");
+
 const PORT = process.env.PORT || 5000;
 
-const startServer = async () => {
-    try {
-        await connectDB();
+process.on("uncaughtException", (err) => {
+  console.error("UNCAUGHT EXCEPTION:");
+  console.error(err);
+});
 
-        app.listen(PORT, () => {
-            console.log(`Server running on port ${PORT}`);
-        });
-    } catch (error) {
-        console.log(error);
-    }
+process.on("unhandledRejection", (err) => {
+  console.error("UNHANDLED REJECTION:");
+  console.error(err);
+});
+
+const startServer = async () => {
+  try {
+    await connectDB();
+
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 startServer();
