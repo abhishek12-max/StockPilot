45c24c9fbb72462fd71a350bad3628bf6b204bf1
abhishek <ap7785980@gmail.com>
2026-08-06 16:14:38 +0530

    Debug Resend

--- Backend/utils/sendMail.js
+++ Backend/utils/sendMail.js
@@ -1,17 +1,22 @@
 const { Resend } = require("resend");
+
+console.log("===== RESEND FILE LOADED =====");
+console.log("API KEY EXISTS:", !!process.env.RESEND_API_KEY);
+
 const resend = new Resend(process.env.RESEND_API_KEY);
+
 const sendMail = async (email, subject, html) => {
-  try {
-    await resend.emails.send({
-      from: "TradeX <onboarding@resend.dev>",
-      to: email,
-      subject,
-      html,
-    });
-  } catch (error) {
-    throw new Error(error.message);
-  }
+  console.log("Sending email to:", email);
+
+  const result = await resend.emails.send({
+    from: "TradeX <onboarding@resend.dev>",
+    to: email,
+    subject,
+    html,
+  });
+
+  console.log("RESEND RESULT:", result);
+  return result;
 };
 
 module.exports = sendMail;
-
