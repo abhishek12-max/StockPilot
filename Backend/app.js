0b3eba74ea1a1b132c929e5a0a4d6927e2adf0dc
abhishek <ap7785980@gmail.com>
2026-08-06 14:16:01 +0530

    Fix CORS for Vercel

--- Backend/app.js
+++ Backend/app.js
@@ -17,10 +17,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(
-    cors({
-        origin: "http://localhost:5173",
-        credentials: true,
-    })
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://trade-x-khaki.vercel.app",
+    ],
+    credentials: true,
+  })
 );
 app.use("/api/auth", authRoutes);
 
